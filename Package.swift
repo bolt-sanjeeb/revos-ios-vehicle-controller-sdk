65357dbe2feb679fc04004285a28a3030395a769
Sanjeeb <sanjeeb.pallai@revos.in>
2022-02-03 11:47:49 +0530

    Package updated

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "0.50.0")),
     ],
     targets: [
-        .binaryTarget(name: "VehicleControllerSDK", url: "https://bitbucket.org/revos-sanjeeb/revos-ios-vehicle-controller-sdk/src/master/VehicleControllerSDK.xcframework.zip", checksum: "ef84537b7aedb3f3332d9004df8beef5d115afdb2d901770bdb2b0c5fd49d334")
+        .binaryTarget(name: "VehicleControllerSDK", url: "https://github.com/revos-sanjeeb/revos-ios-vehicle-controller-sdk/blob/main/VehicleControllerSDK.xcframework.zip", checksum: "ef84537b7aedb3f3332d9004df8beef5d115afdb2d901770bdb2b0c5fd49d334")
         
     ]
 )
